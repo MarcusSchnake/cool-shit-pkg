843a5591d2e8c0748f7c2fb9ee2f710a5947d1e2
Marcus A. Schnake <marcus.schnake@gmail.com>
2022-05-07 20:46:53 -0400

    testing for quotes added

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,22 @@
-const { log } = require("../lib/index.js");
+const { log,getQuote } = require("../lib/index.js");
+
+
+const mockQuotes = [{
+    "quote": "I have nipples, Greg. Could you milk me?",
+    "id": 1,
+    "movie": "Meet The Parents"
+  },
+  {
+    "quote": "Don’t let yourself get attached to anything you are not willing to walk out on in 30 seconds flat if you feel the heat coming around the corner",
+    "id": 2,
+    "movie": "Heat"
+  },
+  {
+    "quote": "Hey, kid, I think I got a little problem here",
+    "id": 3,
+    "movie": "Backdraft"
+  }
+];
 
 describe("my first test suite", () => {
   test("my first test", () => {
@@ -8,4 +26,12 @@ describe("my first test suite", () => {
   test("my second test", () => {
     expect(log("hello")).toBe("hello");
   });
+  
 });
+
+describe("deniro quotes test suite", () => {
+    test("get single quote", () => {
+        expect(JSON.stringify(getQuote(1))).toBe(JSON.stringify(mockQuotes[0]))
+    });
+})
+
